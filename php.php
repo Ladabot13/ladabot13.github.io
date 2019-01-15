3573199fd00e6fdf583fb32a5820d5b97a04418d
Ladabot13 <meredithlada@gmail.com>
2019-01-15 13:34:40 -0800

    Add files via upload

--- php.php
+++ php.php
@@ -1,18 +1,11 @@
 <?php
 
-
+if($_POST){
 $name = $_POST['name'];
 $email = $_POST['email'];
-$date = $_POST['date'];
-$subject = $_POST['subject'];
-$message = $_POST['message'];
-$phone = $_POST['phone'];
-
-$email = "meredithlada@gmail.com";
-$subject = "Event Inquiry: " + $_POST['subject'];
+$subject = "RAK EM UP";
 $message = "$name, you have been challeneged to a game of pool!";
 
-
-mail("meredithlada@gmail.com",$subject,$message);
-
+mail($email,$subject,$message);
+}
 ?>
